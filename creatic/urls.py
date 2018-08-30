b1498405835f12b3f583f4e3ac00c645d8ba3d4d
Jair Buitron <jairnet5@gmail.com>
2018-08-30 12:17:57 -0500

    correccion urls

--- creatic/urls.py
+++ creatic/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('', include('apps.snippets.urls')),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
