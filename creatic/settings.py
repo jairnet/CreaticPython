dacae9fc4941cb7716b3a2662c73b3cbb3b45ef6
jairnet <jairnet5@gmail.com>
2018-10-04 01:14:33 -0500

    change login

--- creatic/settings.py
+++ creatic/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') ###mirar si funciona en heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') ###mirar si funciona en heroku
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
